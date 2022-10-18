b24e59c880c5a107f0c3335955e0298ff828fa1d
Randy Chang <waterboyop@gmail.com>
2022-10-18 15:03:58 +0800

    Refactor code into pileline design

--- yt_contate/main.py
+++ yt_contate/main.py
@@ -1,36 +1,21 @@
-import urllib.request
-import json
-from yt_contate.settings import API_KEY  # 相對路徑
+from yt_contate.pipeline.steps.get_video_list import GetVideoList
+from yt_contate.pipeline.steps.step import StepException
+from yt_contate.pipeline.pileline import Pileline
 
 CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA'
-print(API_KEY)
 
 
-def get_all_video_in_channel(channel_id):
-    # api_key = 'AIzaSyBb23TtLuIiHC6AXJU2X69I_J1eRqdziX4'
+def main():
+    inputs = {
+        'channel_id': CHANNEL_ID
+    }
+    steps = [
+        GetVideoList(),
+    ]
 
-    base_video_url = 'https://www.youtube.com/watch?v='
-    base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
+    p = Pileline(steps)
+    p.run(inputs)
 
-    first_url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(api_key,
-                                                                                                        channel_id)
 
-    video_links = []
-    url = first_url
-    while True:
-        inp = urllib.request.urlopen(url)
-        resp = json.load(inp)
-
-        for i in resp['items']:
-            if i['id']['kind'] == "youtube#video":
-                video_links.append(base_video_url + i['id']['videoId'])
-
-        try:
-            next_page_token = resp['nextPageToken']
-            url = first_url + '&pageToken={}'.format(next_page_token)
-        except KeyError:
-            break
-    return video_links
-
-# video_list = get_all_video_in_channel(CHANNEL_ID)
-# print(video_list)
+if __name__ == '__main__':
+    main()
